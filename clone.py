5e869f81782fa2ab06f917f00021273944587e0f
Steven Eisinger <etienneshorty@gmail.com>
2017-06-22 22:51:40 +0000

    Retrain, modify drive.py

--- clone.py
+++ clone.py
@@ -27,6 +27,17 @@ for line in lines:
 # Split the data set into training and validation
 train_set, valdation_set = train_test_split(data_set, test_size=0.2)
 
+bin_angles = [0, 0, 0]
+for data in train_set:
+    if data[1] > 0:
+        bin_angles[0] += 1
+    elif data[1] < 0:
+        bin_angles[2] += 1
+    else:
+        bin_angles[1] += 1
+        
+print(bin_angles)
+
 # Generate a batch of images and measurements (tuple with image, measurement)
 def batch_gen(samples, batch_size):
     n_samples = len(samples)
@@ -40,6 +51,7 @@ def batch_gen(samples, batch_size):
                 # get image path from line
                 image = cv2.imread(sample[0])
                 # image = cv2.resize(image, (224, 112))
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
                 measurement = sample[1]
                 if sample[2]:
                     image = cv2.flip(image, 1)
@@ -58,34 +70,37 @@ valid_gen = batch_gen(valdation_set, batch_size)
 train_steps = len(train_set)
 valid_steps = len(valdation_set)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation, Dropout
-from keras.layers.convolutional import Convolution2D, ZeroPadding2D
-from keras.layers.pooling import MaxPooling2D
-from keras.layers import Cropping2D
+train = True
 
-# TODO: Try a different architecture
-model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Convolution2D(8,5,5,activation='elu',border_mode='valid',subsample=(2, 2)))
-model.add(Convolution2D(16,5,5,activation='elu',border_mode='valid',subsample=(2, 2)))
-model.add(Convolution2D(32,5,5,activation='elu',border_mode='valid',subsample=(2, 2)))
-model.add(Convolution2D(64,3,3,activation='elu',border_mode='valid'))
-model.add(Convolution2D(64,3,3,activation='elu',border_mode='valid'))
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(60))
-model.add(Dense(30))
-model.add(Dense(1))
+if train:
+    from keras.models import Sequential
+    from keras.layers import Flatten, Dense, Lambda, Activation, Dropout
+    from keras.layers.convolutional import Convolution2D, ZeroPadding2D
+    from keras.layers.pooling import MaxPooling2D
+    from keras.layers import Cropping2D
 
-model.compile(loss='mse', optimizer='adam')
+    # TODO: Try a different architecture
+    model = Sequential()
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+    model.add(Convolution2D(8,5,5,activation='relu',border_mode='valid',subsample=(2, 2)))
+    model.add(Convolution2D(16,5,5,activation='relu',border_mode='valid',subsample=(2, 2)))
+    model.add(Convolution2D(32,5,5,activation='relu',border_mode='valid',subsample=(2, 2)))
+    model.add(Convolution2D(64,3,3,activation='relu',border_mode='valid'))
+    model.add(Convolution2D(64,3,3,activation='relu',border_mode='valid'))
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(60))
+    model.add(Dense(30))
+    model.add(Dense(1))
 
-# Debug
-for layer in model.layers:
-    print('Layer %s: %s -> %s' % (str(type(layer)), str(layer.input_shape), str(layer.output_shape)))
+    model.compile(loss='mse', optimizer='adam')
 
-model.fit_generator(train_gen, samples_per_epoch=train_steps, validation_data=valid_gen, nb_val_samples=valid_steps, nb_epoch=5, max_q_size=1, nb_worker=1)
+    # Debug
+    for layer in model.layers:
+        print('Layer %s: %s -> %s' % (str(type(layer)), str(layer.input_shape), str(layer.output_shape)))
 
-model.save('model.h5')
+    model.fit_generator(train_gen, samples_per_epoch=train_steps, validation_data=valid_gen, nb_val_samples=valid_steps, nb_epoch=5, max_q_size=1, nb_worker=1)
+
+    model.save('model.h5')
 sys.exit()
